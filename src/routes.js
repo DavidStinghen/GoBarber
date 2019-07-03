2fe853e6691e9e837961805670b6344651eca158
DavidNatanael <davidstinghen@gmail.com>
2019-07-03 18:15:17 -0300

    fet: add store route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
+
+// store route
+routes.post('/users', UserController.store);
 
 export default routes;
