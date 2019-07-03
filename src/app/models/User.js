5e49408793ba9a5e6daad857e294f712f9f7b0bc
DavidNatanael <davidstinghen@gmail.com>
2019-07-03 19:07:49 -0300

    fet: add hook to encrypt password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,33 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
+  /**
+   * create method init
+   * use (password: Sequelize.VIRTUAL) to create a vitual field
+   */
   static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
-      provider: Sequelize.STRING,
-    },
-    {
-      sequelize,
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // hook to create a hash of password in password_hash before save
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
     });
+
+    return this;
   }
 }
 
