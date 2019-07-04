431f45d9e67f8b541a563697036b64c895665872
DavidNatanael <davidstinghen@gmail.com>
2019-07-04 01:42:42 -0300

    fet: add route to create a user session

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-// store route
+// user store route
 routes.post('/users', UserController.store);
+
+// session store route
+routes.post('/session', SessionController.store);
 
 export default routes;
