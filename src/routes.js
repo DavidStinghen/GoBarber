ae37d2ebb85d449ae7834d5bd73fec87ee501b6b
DavidNatanael <davidstinghen@gmail.com>
2019-07-04 04:04:05 -0300

    fet: create update route and use auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 // user store route
 routes.post('/users', UserController.store);
-
 // session store route
 routes.post('/session', SessionController.store);
+
+// using middleware to authenticate session
+routes.use(authMiddleware);
+
+// user update data route
+routes.put('/users', UserController.update);
 
 export default routes;
