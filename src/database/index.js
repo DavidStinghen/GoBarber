03c46ce89f25044e61f6d5d0259e0c748ef4a741
DavidNatanael <davidstinghen@gmail.com>
2019-07-09 15:29:00 -0300

    fet: create a loader of associations

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import dataBaseConfig from '../config/database';
 import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -10,8 +11,14 @@ class Database {
   }
 
   init() {
+    // load database
     this.connection = new Sequelize(dataBaseConfig);
-    models.map(model => model.init(this.connection));
+    
+    models
+      // load models
+      .map(model => model.init(this.connection))
+      // load models association
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
