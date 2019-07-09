9e33ce08acff05f33a2c0ad94c3849a93ff38645
DavidNatanael <davidstinghen@gmail.com>
2019-07-09 20:27:01 -0300

    fet: add route to list all providers

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,5 +24,8 @@ routes.put('/users', UserController.update);
 
 // route to upload file
 routes.post('/files', upload.single('file'), FileController.store);
+
+// route to index all providers
+routes.get('/providers', ProviderController.index);
 
 export default routes;
