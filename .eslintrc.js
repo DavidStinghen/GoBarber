b63b78a8264fd25b95fddbd6fcfb71361c7fd562
DavidNatanael <davidstinghen@gmail.com>
2019-07-09 15:25:01 -0300

    fet: changed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
-    "linebreak-style": 0,
-    "global-require": 0,
-    "eslint linebreak-style": [0, "error", "windows"],
-    "prettier/prettier": "error"
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
 };
