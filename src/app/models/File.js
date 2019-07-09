ec3b94360c91626da1f26926c3b01d45a2ec5376
DavidNatanael <davidstinghen@gmail.com>
2019-07-09 20:25:18 -0300

    fet: add url virtual field

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,13 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        // virtual field to provide url to frontend
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
