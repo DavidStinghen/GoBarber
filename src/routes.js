186c9ec10d852f0491fe3e0b3b7c9cff0bd16140
DavidNatanael <davidstinghen@gmail.com>
2019-07-10 00:06:30 -0300

    fet: create routes to store and list appointments

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,10 +23,15 @@ routes.use(authMiddleware);
 // user update data route
 routes.put('/users', UserController.update);
 
-// route to upload file
-routes.post('/files', upload.single('file'), FileController.store);
-
 // route to index all providers
 routes.get('/providers', ProviderController.index);
+
+// route to create a new appointment
+routes.post('/appointments', AppointmentController.store);
+// route to list all appointments
+routes.get('/appointments', AppointmentController.index);
+
+// route to upload file
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
