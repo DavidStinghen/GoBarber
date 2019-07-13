f4b30cb8e82d092c6ffe0770e0a860eed04c843f
DavidNatanael <davidstinghen@gmail.com>
2019-07-13 13:05:42 -0300

    fet: add method to delete appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
@@ -122,6 +122,38 @@ class AppointmentController {
 
     return res.json(appointments);
   }
+
+  /**
+   * method to delete a appointment
+   */
+  async delete(req, res) {
+    // check if user is equal user appointment
+    const appointment = await Appointment.findByPk(req.params.id);
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment"
+      });
+    }
+
+    /**
+     * user can cancel appointments 2 hours in advence
+     * check if is before at 2 hours advance
+     */
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance'
+      });
+    }
+
+    // cancel appointment 
+    appointment.canceled_at = new Date();
+    await appointment.save();
+
+    return res.json(appointment);
+  }
 }
+
+
 
 export default new AppointmentController();
