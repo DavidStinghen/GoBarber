f63b396e6be4f01b6b138d6909ab0eda2827e12e
DavidNatanael <davidstinghen@gmail.com>
2019-07-14 11:13:12 -0300

    fet: add method to send mails to provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   /**
@@ -128,7 +129,20 @@ class AppointmentController {
    */
   async delete(req, res) {
     // check if user is equal user appointment
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk( req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email']
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
+        }
+      ],
+    });
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment"
@@ -146,9 +160,24 @@ class AppointmentController {
       });
     }
 
-    // cancel appointment 
+    // cancel appointment
     appointment.canceled_at = new Date();
     await appointment.save();
+
+    // send email to provider
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date,
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          { locale: pt }
+        )
+      }
+    });
 
     return res.json(appointment);
   }
