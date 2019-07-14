4d277bdc7d70bf997109e8fef0cb4f9283d63793
DavidNatanael <davidstinghen@gmail.com>
2019-07-14 14:22:27 -0300

    fet: show  if is past date && if appointment it's cancelable

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -101,7 +101,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       // limit of page
       limit: 20,
       offset: (page -1) * 20,
