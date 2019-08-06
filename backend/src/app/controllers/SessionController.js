72f8dfc23e7706a472419147f5d85c28336c1c4d
DavidStinghen <davidstinghen@gmail.com>
2019-08-06 18:08:35 -0300

    chore: include avatar provider in reponse

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -27,7 +28,17 @@ class SessionController {
     const { email, password } = req.body;
 
     // verify if email exists
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
     if (!user) {
       return res.status(401).json({ error: 'User not found!' });
     }
@@ -37,10 +48,10 @@ class SessionController {
     }
 
     // if all is ok responde with token to auth in session
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
