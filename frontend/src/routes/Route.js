c1b1c3068bae96c950b85c19d9370333cced942e
DavidStinghen <davidstinghen@gmail.com>
2019-08-08 19:59:05 -0300

    fix: root import

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '~/pages/_layouts/auth';
-import DefaultLayout from '~/pages/_layouts/default';
-import { store } from '~/store';
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+import { store } from '../store';
 
 export default function RouterWrapper({
   component: Component,
