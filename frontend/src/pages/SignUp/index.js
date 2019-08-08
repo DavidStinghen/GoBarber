161a3e8bb3afd76bc48e0e0816cdc68224c8cd6e
DavidStinghen <davidstinghen@gmail.com>
2019-08-08 11:22:26 -0300

    chore: add data validation

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Você deve informar seu nome'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Você deve informar seu e-mail'),
+  password: Yup.string()
+    .min(8, 'A senha deve ter no mínimo 8 caracteres')
+    .required('Vocẽ deve informar sua senha'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="InForme seu nome completo" />
         <Input name="email" type="email" placeholder="Informe seu e-mail" />
         <Input
