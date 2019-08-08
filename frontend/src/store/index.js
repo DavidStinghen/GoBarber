1a9f48683e786ecb9b8ea7de70f85e222c80366f
DavidStinghen <davidstinghen@gmail.com>
2019-08-08 20:00:19 -0300

    fix: reactotron + saga bug

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,22 +1,22 @@
-import createSagaMiddleware from 'redux-saga';
 import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
 
 import createStore from './createStore';
+import persistReducers from './persistReducers';
+
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
-import presistReducers from './persistReducers';
 
 const sagaMonitor =
   process.env.NODE_ENV === 'development'
     ? console.tron.createSagaMonitor()
     : null;
 
-const sagaMiddleware = createSagaMiddleware(sagaMonitor);
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(presistReducers(rootReducer), middlewares);
-
+const store = createStore(persistReducers(rootReducer), middlewares);
 const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
