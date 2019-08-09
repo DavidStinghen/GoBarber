94f996e39bc8a0ac9064f9b7eb15a0a3e7743499
DavidStinghen <davidstinghen@gmail.com>
2019-08-09 20:13:23 -0300

    feature: update provider avatar

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { updateProfileRequest } from '../../store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Informe seu nome completo" />
         <Input name="email" type="email" placeholder="Informe seu e-mail" />
 
