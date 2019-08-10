5863242f5762105485e09394fb5791e7c22e1b48
DavidStinghen <davidstinghen@gmail.com>
2019-08-10 19:05:06 -0300

    feature: create a schedule page

--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 120%;
+  height: 150%;
   background: linear-gradient(-110deg, #3b9eff, #7159c1, #ab59c1);
 `;
