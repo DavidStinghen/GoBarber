cb47eced898bf941074c0384cbbf61237cb42c61
DavidStinghen <davidstinghen@gmail.com>
2020-03-01 11:53:32 -0300

    feature: create SignUp methods in page

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
 import Background from '~/components/Background';
 import logo from '~/assets/logo.png';
 
 import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
 export default function SignUp({ navigation }) {
+  const dispatch    = useDispatch();
+  const emailRef    = useRef();
+  const passwordRef = useRef();
+
+  const [name,     setName]     = useState('');
+  const [email,    setEmail]    = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <Background>
       <Container>
@@ -18,6 +34,10 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCaptalize="none"
             placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
 
           <FormInput
@@ -25,15 +45,25 @@ export default function SignUp({ navigation }) {
             keyBoardType="email-address"
             autoCaptalize="none"
             placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={() => {}}>Cadastrar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>Cadastrar</SubmitButton>
         </Form>
 
         <SignLink onPress={() => { navigation.navigate('SignIn')}}>
